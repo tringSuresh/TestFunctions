f543baeb01ce120f5e69bc03299d3906f0a7c0a3
tringSuresh <suresh.r@tringapps.com>
2017-12-18 12:07:00 +0530

    Initial Commit

---
+++ TestFunctions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFunctions
+//
+//  Created by Suresh R on 18/12/17.
+//  Copyright © 2017 Tringapps Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
